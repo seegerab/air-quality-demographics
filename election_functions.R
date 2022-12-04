88f5d00ca57f05d4188671f89d02ef187dcb8743
Abigail Seeger <seegerab@umich.edu>
2022-12-03 23:05:00 -0500

    Bin all parties not Democrat/Republican into an Other category

--- election_functions.R
+++ election_functions.R
@@ -8,31 +8,41 @@ extract_election <- function(year_input, state_input){
   ### Filter the data to only general elections that aren't
   ### special elections for the U.S. house and only the 
   ### Democratic and Republican parties
+  
+  ### Bin all parties not DEMOCRAT or REPUBLICAN to OTHER
+  data$party <- ifelse(data$party %in% c("DEMOCRAT", "REPUBLICAN"), data$party, "OTHER")
+  total_district <- data%>%
+    select(year, state, district, totalvotes)%>%
+    filter(year == year_input, state == state_input)%>%
+    distinct()
   data <- data%>%
-    filter(party %in% c("DEMOCRAT", "REPUBLICAN"))%>%
     filter(special == "FALSE")%>%
     filter(office == "US HOUSE")%>%
     filter(stage == "GEN")%>%
     filter(year == year_input)%>%
     filter(state == state_input)%>%
+    group_by(year, state, district, party)%>%
+    summarize(candidatevotes = sum(candidatevotes))
+  data <- left_join(data, total_district, by = "district")
+  data <- data%>% 
     mutate(vote_prop = candidatevotes / totalvotes)%>%
-    select(c(year, state, district, party, vote_prop, candidatevotes, totalvotes))
+    select(c(year.x, state.x, district, party, vote_prop, candidatevotes, totalvotes, state.y, year.y))
   ### Add a column with the shape information
   geometry <- congressional_districts(state = toupper(substr(state_input, 1, 2)), year = year_input)%>%
     select(geometry, NAMELSAD)
   ### Extract district from the full district name
   geometry$district <- as.integer(sapply(strsplit(geometry$NAMELSAD, "Congressional District "), "[[", 2))
-  merged_data <- left_join(data, geometry, by = "district")
-  merged_data <- merged_data%>%
-    filter(party == "DEMOCRAT")
+  merged_data <- left_join(data, geometry, by = "district")%>%
+    select(-c(state.y, year.y))
+  colnames(merged_data)[1:2] <- c("year", "state")
   ### Should figure out a better way to visualize the data...
   return(merged_data)
 }
 
 ### Test the function
-test <- extract_election(2014, "OREGON")
+test <- extract_election(2014, "CALIFORNIA")
 
-ggplot(data = test, aes(geometry = geometry))+
+ggplot(data = extract_election(2020, "CALIFORNIA")%>%filter(party == "DEMOCRAT"), aes(geometry = geometry))+
   geom_sf(aes(fill=vote_prop))+
   labs(fill = "Percent of the vote the\nDemocratic candidate recieved", title = "2012 U.S. House of Representatives Results")+
   scale_fill_gradient2(low="red", midpoint = 0.5, high="blue")+
